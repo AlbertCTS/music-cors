bc172d51714d0dc3ac700a6fe8451626d74be18f
Albert Chen <Donglin.Chen@cognizant.com>
2023-05-18 16:35:57 +1000

    feat: create a Middleware for music-festival

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+var cors = require('cors')
+const app = express();
+app.use(cors());
+const { createProxyMiddleware } = require('http-proxy-middleware');
+app.use('/codingtest/api/v1/festivals', createProxyMiddleware({ 
+    target: 'https://eacp.energyaustralia.com.au/', //original url
+    changeOrigin: true, 
+    //secure: false,
+    onProxyRes: function (proxyRes, req, res) {
+       proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+    }
+}));
+app.listen(5000);
